21c489f8b8559684dd4d9862fae7daee23a1d5cd
Shuhao Zhang <shuhao.qgg.zhang@gmail.com>
2025-06-28 17:46:35 -0400

    chore: scaffold src/integrations/openai/OpenAIApiIntegration.test.ts

---
+++ src/integrations/openai/OpenAIApiIntegration.test.ts
@@ -0,0 +1,18 @@
+import { TestBed } from '@angular/core/testing';
+import { OpenAIApiService } from './OpenAIApiService';
+
+describe('OpenAIApiIntegration', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      providers: [OpenAIApiService]
+    });
+  });
+
+  it('should be created', () => {
+    // TODO: Implement OpenAIApiService creation test
+  });
+
+  it('should call OpenAIApiService', () => {
+    // TODO: Implement OpenAIApiService call test
+  });
+});
