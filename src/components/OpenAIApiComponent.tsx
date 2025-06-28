4c1932cd32e01885cd0f984983fd804e9cc600bd
Shuhao Zhang <shuhao.qgg.zhang@gmail.com>
2025-06-28 17:46:34 -0400

    chore: scaffold src/components/OpenAIApiComponent.tsx

---
+++ src/components/OpenAIApiComponent.tsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import { OpenAIApi } from "@openai/openai-api-typescript";
+
+// TODO: Initialize OpenAIApi instance
+// TODO: Implement API call to OpenAI
+export const OpenAIApiComponent = () => {
+  const [response, setResponse] = useState(null);
+  useEffect(() => {
+    // TODO: Make API call to OpenAI
+    // TODO: Handle API response
+  }, []);
+  return <div>TODO</div>;
+};
